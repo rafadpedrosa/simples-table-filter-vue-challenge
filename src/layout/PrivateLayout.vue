cce501b3117f6873ae3e4937ad9f50690c80f41e
rafadpedrosa <rafadpedrosa@gmail.com>
2022-05-08 14:33:15 -0300

    [#0] Create initial structure

---
+++ src/layout/PrivateLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="private-layout container-fluid">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PrivateLayout'
+}
+</script>
+
+<style lang="scss" scoped>
+  .private-layout {
+    padding-top: 3rem;
+  }
+</style>
