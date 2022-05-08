c3c7e8ebc40934ff9dd4dda5a875fa86d39e10db
rafadpedrosa <rafadpedrosa@gmail.com>
2022-05-08 14:34:27 -0300

    [#0] Add components to tabs; Use the Layout into the home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,50 @@
 <template>
   <div class="home">
+    <!-- ::: I'll not over think this. I would/could probably use routers or a code calling a method to change this name dynamically but I would like to keep it simple and explain during next interview if needed. -->
+    <h1 class="pb-3"> Members </h1>
     <!-- Your implementation should start here -->
-    <h4>[Members Page]</h4>
+    <b-tabs content-class="mt-3">
+      <b-tab title="MEMBERS" title-link-class="tab-title" active lazy>
+        <members></members>
+      </b-tab>
+      <b-tab title="INVITES" title-link-class="tab-title" disabled lazy>
+        <invites></invites>
+      </b-tab>
+    </b-tabs>
   </div>
 </template>
 
 <script>
+import Members from '@/components/Members'
+import Invites from '@/components/Invites'
+
 export default {
+  components: {
+    Members,
+    Invites
+  },
   name: 'Home'
 }
 </script>
+
+<style lang="scss">
+ul.nav.nav-tabs {
+  border: 0;
+}
+
+ul.nav > li.nav-item {
+  .tab-title {
+    border: none;
+    border-radius: 0;
+    border-bottom: 2px solid $gray-300;
+  }
+  .tab-title.active {
+    color: $primary;
+    font-weight: bold;
+    border-color: $primary;
+    border-bottom: 3px solid $primary;
+    padding-bottom: 6px;
+  }
+}
+
+</style>
