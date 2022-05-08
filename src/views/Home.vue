7f3de2b89b3967b0390d48803cbe5f3e1a33093d
rafadpedrosa <rafadpedrosa@gmail.com>
2022-05-08 16:26:12 -0300

    [#0] Add filter search member to the screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,17 @@
     <h1 class="pb-3 mx-3"> Members </h1>
     <!-- Your implementation should start here -->
     <b-tabs class="mx-3"  content-class="mt-3">
+      <template #tabs-end>
+        <li class="nav-item tab-input-filter pr-3">
+          <b-input-group size="sm" >
+            <b-input-group-prepend is-text>
+              <b-icon icon="search" font-scale="1.5"></b-icon>
+            </b-input-group-prepend>
+            <b-form-input type="search" placeholder="Search members"></b-form-input>
+          </b-input-group>
+        </li>
+      </template>
+
       <b-tab title="MEMBERS" title-link-class="tab-title" active lazy>
         <members></members>
       </b-tab>
@@ -45,6 +56,28 @@ ul.nav > li.nav-item {
     border-bottom: 3px solid $primary;
     padding-bottom: 6px;
   }
+}
+
+.tab-input-filter {
+  position: absolute;
+  right: 0;
+  border-radius: 1rem;
+
+    .input-group {
+      .input-group-prepend {
+        .input-group-text {
+          border-radius: 5rem 0 0 5rem;
+          background-color: white;
+          padding-left: 1rem;
+        }
+      }
+
+      input {
+        border-left: none;
+        border-radius: 5rem;
+        width: 20rem;
+      }
+    }
 }
 
 </style>
