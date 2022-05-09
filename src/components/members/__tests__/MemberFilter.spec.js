e982e2a5fabf631ed058ca8c79efa34e9e32cd85
rafadpedrosa <rafadpedrosa@gmail.com>
2022-05-08 21:33:00 -0300

    [#0] Improve tests configurations

--- src/components/members/__tests__/MemberFilter.spec.js
+++ src/components/members/__tests__/MemberFilter.spec.js
@@ -1,12 +1,13 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import MembersFilter from '@/components/members/MembersFilter'
 
-describe('HelloWorld.vue', () => {
+describe('MembersFilter.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(MembersFilter, {
       propsData: { msg }
     })
+
     expect(wrapper.text()).toMatch(msg)
   })
 })
